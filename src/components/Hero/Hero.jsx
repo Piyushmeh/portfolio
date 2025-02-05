67b43c460ad16535607edb512157ad9a6f07734d
Piyush <piyushbansod006@gmail.com>
2025-02-05 12:27:13 +0530

    Initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Piyush</h1>
+        <p className={styles.description}>
+          I'm a software developer, I specialize in full-stack development Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:piyushmehrotra006@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroimage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
